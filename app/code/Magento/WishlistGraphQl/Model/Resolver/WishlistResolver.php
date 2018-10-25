b4ed1a5eddb56fdf429df83879b3fd6c0ab3c73c
David Verholen <david@verholen.com>
2018-10-25 16:22:33 +0200

    #43 add wishlist type and basic resolver

---
+++ app/code/Magento/WishlistGraphQl/Model/Resolver/WishlistResolver.php
@@ -0,0 +1,71 @@
+<?php
+declare(strict_types=1);
+/**
+ * WishlistItemTypeResolver
+ *
+ * @copyright Copyright © 2018 brandung GmbH & Co. KG. All rights reserved.
+ * @author    david.verholen@brandung.de
+ */
+
+namespace Magento\WishlistGraphQl\Model\Resolver;
+
+use Magento\Customer\Api\Data\CustomerInterface;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Query\Resolver\ContextInterface;
+use Magento\Framework\GraphQl\Query\Resolver\Value;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Magento\Wishlist\Model\Item;
+use Magento\Wishlist\Model\ResourceModel\Wishlist;
+use Magento\Wishlist\Model\WishlistFactory;
+
+class WishlistResolver implements ResolverInterface
+{
+    /**
+     * @var Wishlist
+     */
+    private $wishlistResource;
+    /**
+     * @var WishlistFactory
+     */
+    private $wishlistFactory;
+
+    public function __construct(Wishlist $wishlistResource, WishlistFactory $wishlistFactory)
+    {
+        $this->wishlistResource = $wishlistResource;
+        $this->wishlistFactory = $wishlistFactory;
+    }
+
+
+    /**
+     * Fetches the data from persistence models and format it according to the GraphQL schema.
+     *
+     * @param \Magento\Framework\GraphQl\Config\Element\Field $field
+     * @param ContextInterface $context
+     * @param ResolveInfo $info
+     * @param array|null $value
+     * @param array|null $args
+     * @throws \Exception
+     * @return mixed|Value
+     */
+    public function resolve(Field $field,
+                            $context,
+                            ResolveInfo $info,
+                            array $value = null,
+                            array $args = null)
+    {
+        $customerId = $context->getUserId();
+
+        /** @var \Magento\Wishlist\Model\Wishlist $wishlist */
+        $wishlist = $this->wishlistFactory->create();
+        $this->wishlistResource->load($wishlist, $customerId, 'customer_id');
+        return [
+            'items' => array_map(function (Item $item) {
+                return [
+                    'id' => $item->getId(),
+                    'qty' => $item->getData('qty')
+                ];
+            }, $wishlist->getItemCollection()->getItems())
+        ];
+    }
+}
